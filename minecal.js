8825959a3954d96512f9dabb6abf922a8ba8a711
guyssarAPI <134362598+guyssarAPI@users.noreply.github.com>
2023-05-23 13:18:32 +0700

    Update minecal.js

--- minecal.js
+++ minecal.js
@@ -1,12 +1,85 @@
     //cal block/ingot/nugget to block/ingot/nugget
-    function calculate_ore_block() {var blockore = parseInt(document.getElementById('blockore').value);var result = blockore * 9;document.getElementById('result').innerHTML = result;var result2 = result *9;document.getElementById('result2').innerHTML = result2;}
-    function calculate_ore_Ingot() {var blockore2 = parseInt(document.getElementById('blockore2').value);if(blockore2 < 9){result3 = 0;document.getElementById('result3').innerHTML = result3;}else{var calresult3 = blockore2 / 9;var result3 = Math.floor(calresult3);document.getElementById('result3').innerHTML = result3;}var calresult4 = blockore2 *9;var result4 = Math.floor(calresult4);document.getElementById('result4').innerHTML = result4;}
-    function calculate_ore_Nugget() {var blockore3 = parseInt(document.getElementById('blockore3').value);if(blockore3 < 81){result5 = 0;document.getElementById('result5').innerHTML = result5;}else{var calresult5 = blockore3 / 9 / 9 ;var result5 = Math.floor(calresult5);document.getElementById('result5').innerHTML = result5;}if(blockore3 < 9){result6 = 0;document.getElementById('result6').innerHTML = result6;}else{var calresult6 = blockore3 / 9  ;var result6 = Math.floor(calresult6);document.getElementById('result6').innerHTML = result6;}}
+        //block to ingot/nugget
+    function calculate_ore_block() {
+        var blockore = parseInt(document.getElementById('blockore').value);
+
+        var result = blockore * 9;
+        document.getElementById('result').innerHTML = result;
+
+        var result2 = result *9;
+        document.getElementById('result2').innerHTML = result2;
+    }
+        //block to ingot/nugget
+
+        //ingot to block/nugget
+    function calculate_ore_Ingot() {
+        var blockore2 = parseInt(document.getElementById('blockore2').value);
+
+        if(blockore2 < 9){
+            result3 = 0;
+            document.getElementById('result3').innerHTML = result3;
+        }
+        else{
+            var calresult3 = blockore2 / 9;
+            var result3 = Math.floor(calresult3);
+            document.getElementById('result3').innerHTML = result3;
+        }
+        
+        var calresult4 = blockore2 *9;
+        var result4 = Math.floor(calresult4);
+        document.getElementById('result4').innerHTML = result4;
+    }
+        //ingot to block/nugget
+
+        //nugget to block/ingot
+    function calculate_ore_Nugget() {
+        var blockore3 = parseInt(document.getElementById('blockore3').value);
+        
+        if(blockore3 < 81){
+            result5 = 0;
+            document.getElementById('result5').innerHTML = result5;
+        }
+        else{
+            var calresult5 = blockore3 / 9 / 9 ;
+            var result5 = Math.floor(calresult5);
+            
+            document.getElementById('result5').innerHTML = result5;
+        }
+        
+        if(blockore3 < 9){
+            result6 = 0;
+            document.getElementById('result6').innerHTML = result6;
+        }
+        else{
+            var calresult6 = blockore3 / 9  ;
+            var result6 = Math.floor(calresult6);
+            
+            document.getElementById('result6').innerHTML = result6;
+        }
+    }
+        //nugget to block/ingot
     //cal block/ingot/nugget to block/ingot/nugget
 
     //cal redstone
         //cal PowerRail
-    function calculate_powerrail(){var powerrail = parseInt(document.getElementById('powerrail').value);var powerrail1 = Math.floor(powerrail);var powerrail_gold = powerrail1 *6;document.getElementById('powerrail_gold').innerHTML = powerrail_gold;var powerrail_real = powerrail1 *6;document.getElementById('powerrail_real').innerHTML = powerrail_real;var powerrail_redstone = powerrail1 *1;document.getElementById('powerrail_redstone').innerHTML = powerrail_redstone;var powerrail_stick = powerrail1 *1;document.getElementById('powerrail_stick').innerHTML = powerrail_stick;}
+    function calculate_powerrail(){
+        var powerrail = parseInt(document.getElementById('powerrail').value);
+        var powerrail1 = Math.floor(powerrail);
+        
+        var powerrail_gold = powerrail1 *6;
+        document.getElementById('powerrail_gold').innerHTML = powerrail_gold;
+        
+        var powerrail_real = powerrail1 *6;
+        document.getElementById('powerrail_real').innerHTML = powerrail_real;
+        
+        var powerrail_redstone = powerrail1 *1;
+        document.getElementById('powerrail_redstone').innerHTML = powerrail_redstone;
+        
+        var powerrail_stick = powerrail1 *1;
+        document.getElementById('powerrail_stick').innerHTML = powerrail_stick;
+    }
+        //cal PowerRail
+        
         //cal Rail
     function calculate_rail(){
         var rail = parseInt(document.getElementById('rail').value); 
